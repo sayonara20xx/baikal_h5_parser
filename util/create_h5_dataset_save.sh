689097856d99ad1f2dcf17c2c883d090f8f32de2
sayonara20xx <danilka.andreev.123@gmail.com>
2022-03-31 13:06:16 +0300

    added new extraction method for additional researches, also added new docker sh script saving h5 files with much less info (i hope its neccessary)

--- util/create_h5_dataset_save.sh
+++ util/create_h5_dataset_save.sh
@@ -2,11 +2,14 @@
 
 sudo docker run --name baikal_gen_auto -d -it git.jinr.ru:5005/baikal/generators:latest bash
 
-echo "copying config file inside container:"
+echo "copying config files inside container:"
 sudo docker cp ./util/current_conf.mac baikal_gen_auto:/generators/packages/framework/current_config.mac
+sudo docker cp ./util/required_h5Writer_conf.conf baikal_gen_auto:/generators/packages/framework/configs/h5Writer.conf
 
 echo "currently used following '.mac' file:"
 sudo docker exec -it baikal_gen_auto /bin/bash -c 'cd packages/framework; cat current_config.mac'
+echo "currently used following '.conf' h5Writer file:"
+sudo docker exec -it baikal_gen_auto /bin/bash -c 'cd packages/framework/configs; cat h5Writer.conf'
 
 echo "start generating events..."
 sudo docker exec -it baikal_gen_auto /bin/bash -c 'source generators_env.sh; cd packages/framework; python3 ./loop.py --source LiGen --n_events 200 --ligen_macro ./current_config.mac'
